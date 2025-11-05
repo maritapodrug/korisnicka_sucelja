adcd4fb3e6a6c75f880a82f90058a893f031546a
maritapodrug <maritapodrug211@gmail.com>
2025-11-05 12:58:41 +0100

    Adding Next.js aplikaciju

---
+++ escaperoom/app/_components/navigation.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Page = {
+  title: string;
+  path: `/${string}`;
+};
+
+/**
+ * pages is an array of objects representing the pages in the web app.
+ * Each object contains a title and a path. This array is used to generate the navigation menu.
+ *
+ * We hardcode pages here, but in real app you want to store and read this information from some external source (e.g. CMS, DB, config file, etc).
+ */
+const pages: Page[] = [
+  { title: "Home", path: "/" },
+  {
+    title: "Rooms",
+    path: "/rooms",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "Book now",
+    path: "/booknow",
+  },
+  {
+    title: "Contact us",
+    path: "/contact",
+  },
+  {
+    title: "Login/Register", 
+    path: "/login",
+  },
+  {
+    title: "Account", //only if the user is logged in  
+    path: "/account",
+  },
+];
+
+/**
+ * Render a page list item.
+ * @param page - { title, path } for the page
+ * @param index - array index used for key
+ * @returns JSX element for a list item
+ */
+function processPage(page: Page, index: number, currentPath?: string) {
+  return (
+    <li key={index}>
+      <Link
+        href={page.path}
+        className={currentPath === page.path ? "font-extrabold" : ""}
+      >
+        {page.title}
+      </Link>
+    </li>
+  );
+}
+
+export function Navigation() {
+  const currentPath = usePathname();
+  return (
+    <nav>
+      <ul className="flex space-x-4 mb-4">
+        {pages.map((page, index) => processPage(page, index, currentPath))}
+      </ul>
+    </nav>
+  );
+}
