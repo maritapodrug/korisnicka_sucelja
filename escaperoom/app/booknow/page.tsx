97b57a3bf09d912696d4c3ec8b4cf685eb5d29d4
maritapodrug <maritapodrug211@gmail.com>
2025-12-19 18:50:19 +0100

    Update navigation hover styles and blog pagination

--- escaperoom/app/booknow/page.tsx
+++ escaperoom/app/booknow/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
       <Navigation />
-      <h1 className="text-6xl font-extrabold tracking-tight">Bookings</h1>
+      <h1 className="text-6xl font-extrabold tracking-tight">Book now</h1>
     </main>
   );
 }
